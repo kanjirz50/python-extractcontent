76585b1534b12eb38fd3e7b39f15704a0e685db9
Kanji Takahashi <ka.takahashi@sansan.com>
2018-01-31 15:06:37 +0900

    setup

--- setup.py
+++ setup.py
@@ -1,5 +1,17 @@
-from distutils.core import setup
-setup( name = 'extractcontent',
-       version = '0.0.1',
-       py_modules = ['extractcontent'],
-     )
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, "README.rst"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="extractcontent",
+    version="0.0.1",
+    description="",
+    long_description=long_description,
+    url="https://github.com/kanjirz50/python-extractcontent",
+    packages=find_packages(exclude=["contrib", "docs", "tests"]),
+)
